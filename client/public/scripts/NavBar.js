3dc14e5abbc6a9391be9e6fe58c622a8b0ee3c27
Amit Ofir <amit.ofir4@gmail.com>
2023-06-11 15:41:33 +0300

    navBar update + hamburgerMenu

--- client/public/scripts/NavBar.js
+++ client/public/scripts/NavBar.js
@@ -1,7 +1,7 @@
 var searchInput = document.getElementById('search-input');
 var searchIcon = document.getElementById('search-icon');
 
-searchIcon.addEventListener('click', function() {
+searchIcon.addEventListener('click', function () {
   if (searchInput.style.display == 'none') {
     searchInput.style.display = 'block';
     searchInput.focus();
@@ -11,3 +11,18 @@ searchIcon.addEventListener('click', function() {
   }
 });
 
+
+function showNotification(count) {
+  var notification = document.getElementById('notification');
+  var countElement = document.getElementById('count');
+
+  if (count > 0) {
+    countElement.textContent = count;
+    notification.style.display = 'block';
+  } else {
+    notification.style.display = 'none';
+  }
+}
+
+var notificationCount = 2; 
+showNotification(notificationCount);
