d09de070677e4b12cbc8187bfdf99a46f7da1c22
Yehuda Daniel <h10ytguf@gmail.com>
2023-06-24 00:09:23 +0300

    adding registration post route

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const userController = require('../controller/user.controller');
 
 
 const userRouter = express.Router();
 
 userRouter
-    .get('/me', (req, res) => {
+    .get('/', (req, res) => {
         res.send("Hello");
     })
+
+    //-- Post Requests --
+    .post('/register', userController.register_C)
 
 
 module.exports = userRouter;
