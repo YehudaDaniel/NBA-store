891864daf21a1085144812363b2488321c723fef
Yehuda Daniel <h10ytguf@gmail.com>
2023-06-24 00:10:52 +0300

    writing test to the registration route function expecting it not to fail - still failing not finished

---
+++ src/models/Product.model.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true,
+    trim: true
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  category: [{ 
+    type: String, 
+    required: true 
+  }],
+  image: { 
+    type: String 
+  }
+});
+
+// Static method to create a new product
+productSchema.statics.newProduct = function (productData) {
+  const newProduct = new this(productData);
+  return newProduct.save();
+};
+
+// Instance method to change the product's name
+productSchema.methods.changeName = function (newName) {
+  this.name = newName;
+  return this.save();
+};
+
+// Instance method to change the product's price
+productSchema.methods.changePrice = function (newPrice) {
+  this.price = newPrice;
+  return this.save();
+};
+
+// Instance method to change the product's description
+productSchema.methods.changeDescription = function (newDescription) {
+  this.description = newDescription;
+  return this.save();
+};
+
+// Instance method to add a category to the product
+productSchema.methods.addCategory = function (newCategory) {
+  this.category.push(newCategory);
+  return this.save();
+};
+
+// Instance method to remove a category from the product
+productSchema.methods.removeCategory = function (category) {
+  this.category.pull(category);
+  return this.save();
+};
+
+// Instance method to change the product's image
+productSchema.methods.changeImage = function (newImage) {
+  this.image = newImage;
+  return this.save();
+};
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
